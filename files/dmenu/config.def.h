82e50be5022850287cc69328e4f467ca961c17ce
Franek Borowiec <franek.borowiec@gmail.com>
2022-01-03 12:35:21 +0100

    Picom task

--- files/dmenu/config.def.h
+++ files/dmenu/config.def.h
@@ -13,7 +13,7 @@ static const char *colors[SchemeLast][2] = {
     [SchemeOut] = {"#000000", "#00ffff"},
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines = 0;
+static unsigned int lines = 10;
 
 /* -h option; minimum height of a menu line */
 static unsigned int lineheight = 30;
