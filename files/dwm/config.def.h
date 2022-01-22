fac86dc62514c8f8ffa043c5fbfa7119ab72ebd0
Franek Borowiec <franek.borowiec@gmail.com>
2022-01-22 16:42:07 +0100

    DWM config

--- files/dwm/config.def.h
+++ files/dwm/config.def.h
@@ -1,85 +1,228 @@
 /* See LICENSE file for copyright and license details. */
-#include <X11/XF86keysym.h>
 
 /* appearance */
 static const unsigned int borderpx = 1; /* border pixel of windows */
-static const unsigned int gappx = 12;   /* gaps between windows */
 static const unsigned int snap = 32;    /* snap pixel */
-static const int showbar = 1;           /* 0 means no bar */
-static const int topbar = 1;            /* 0 means bottom bar */
-static const int user_bh = 70; /* 0 means that dwm will calculate bar height, >=
-                                  1 means dwm will user_bh as bar height */
+static const char autostartblocksh[] = "autostart_blocking.sh";
+static const char autostartsh[] = "autostart.sh";
+static const char dwmdir[] = "dwm";
+static const char localshare[] = ".local/share";
+static const int showbar = 1; /* 0 means no bar */
+static const int topbar = 1;  /* 0 means bottom bar */
+static const int bar_height =
+    0; /* 0 means derive from font, >= 1 explicit height */
+static const int vertpad = 10; /* vertical padding of bar */
+static const int sidepad = 10; /* horizontal padding of bar */
+/* Status is to be shown on: -1 (all monitors), 0 (a specific monitor by index),
+ * 'A' (active monitor) */
+static const int statusmon = -1;
+static const int horizpadbar = 2; /* horizontal padding for statusbar */
+static const int vertpadbar = 2;  /* vertical padding for statusbar */
 
-static const char *fonts[] = {"monospace:size=10"};
-static const char dmenufont[] = "monospace:size=10";
-static const char col_gray1[] = "#222222";
-static const char col_gray2[] = "#444444";
-static const char col_gray3[] = "#faebd7";
-static const char col_black[] = "#000000";
-static const char col_border[] = "#ff79c6";
-static const char col_cyan[] = "#005577";
-static const char col_red[] = "#ff5555";
-static const char col_yellow[] = "#ffff00";
-static const char col_smthFG[] = "#f5fffa";
-static const char col_SeparatorBG[] = "#707070";
-static const char col_BGgreen[] = "#008000";
-static const char col_smth[] = "#eee5de";
+/* Indicators: see patch/bar_indicators.h for options */
+static int tagindicatortype = INDICATOR_TOP_LEFT_SQUARE;
+static int tiledindicatortype = INDICATOR_NONE;
+static int floatindicatortype = INDICATOR_TOP_LEFT_SQUARE;
+static const char *fonts[] = {"fontawesome:size=10", "JoyPixels:pixelsize=10",
+                              "monospace:size=10"};
+static const char dmenufont[] = "fontawesome:size=10";
+
+static char c000000[] = "#000000"; // placeholder value
+
+static char normfgcolor[] = "#bbbbbb";
+static char normbgcolor[] = "#222222";
+static char normbordercolor[] = "#444444";
+static char normfloatcolor[] = "#db8fd9";
+
+static char selfgcolor[] = "#eeeeee";
+static char selbgcolor[] = "#005577";
+static char selbordercolor[] = "#005577";
+static char selfloatcolor[] = "#005577";
+
+static char titlenormfgcolor[] = "#bbbbbb";
+static char titlenormbgcolor[] = "#222222";
+static char titlenormbordercolor[] = "#444444";
+static char titlenormfloatcolor[] = "#db8fd9";
+
+static char titleselfgcolor[] = "#eeeeee";
+static char titleselbgcolor[] = "#005577";
+static char titleselbordercolor[] = "#005577";
+static char titleselfloatcolor[] = "#005577";
+
+static char tagsnormfgcolor[] = "#bbbbbb";
+static char tagsnormbgcolor[] = "#222222";
+static char tagsnormbordercolor[] = "#444444";
+static char tagsnormfloatcolor[] = "#db8fd9";
+
+static char tagsselfgcolor[] = "#eeeeee";
+static char tagsselbgcolor[] = "#005577";
+static char tagsselbordercolor[] = "#005577";
+static char tagsselfloatcolor[] = "#005577";
+
+static char hidnormfgcolor[] = "#005577";
+static char hidselfgcolor[] = "#227799";
+static char hidnormbgcolor[] = "#222222";
+static char hidselbgcolor[] = "#222222";
+
+static char urgfgcolor[] = "#bbbbbb";
+static char urgbgcolor[] = "#222222";
+static char urgbordercolor[] = "#ff0000";
+static char urgfloatcolor[] = "#db8fd9";
+
 static const unsigned int baralpha = 0xd0;
 static const unsigned int borderalpha = OPAQUE;
-static const char *colors[][3] = {
+static const unsigned int alphas[][3] = {
+    /*                       fg      bg        border     */
     [SchemeNorm] = {OPAQUE, baralpha, borderalpha},
     [SchemeSel] = {OPAQUE, baralpha, borderalpha},
-    [SchemeWarn] = {col_black, col_yellow, col_red},
-    [SchemeUrgent] = {col_smthFG, col_red, col_red},
-    [SchemeTagUnderline] = {col_border, col_border, col_border},
-    [SchemeBGgreen] = {col_gray3, col_BGgreen, col_gray2},
-    [SchemeWnameBG] = {col_border, col_gray1, col_red},
-    [SchemeBlockSeparator] = {col_cyan, col_SeparatorBG, col_red},
-    [SchemeCmusButton] = {col_smth, col_cyan, col_red},
+    [SchemeTitleNorm] = {OPAQUE, baralpha, borderalpha},
+    [SchemeTitleSel] = {OPAQUE, baralpha, borderalpha},
+    [SchemeTagsNorm] = {OPAQUE, baralpha, borderalpha},
+    [SchemeTagsSel] = {OPAQUE, baralpha, borderalpha},
+    [SchemeHidNorm] = {OPAQUE, baralpha, borderalpha},
+    [SchemeHidSel] = {OPAQUE, baralpha, borderalpha},
+    [SchemeUrg] = {OPAQUE, baralpha, borderalpha},
 };
 
-static const char *upvol[] = {"/usr/bin/pactl", "set-sink-volume", "0", "+5%",
-                              NULL};
-static const char *downvol[] = {"/usr/bin/pactl", "set-sink-volume", "0", "-5%",
-                                NULL};
-static const char *mutevol[] = {"/usr/bin/pactl", "set-sink-mute", "0",
-                                "toggle", NULL};
-static const char *briup[] = {"/usr/bin/brightnessctl", "s", "+10%", NULL};
-static const char *bridown[] = {"/usr/bin/brightnessctl", "s", "10%-", NULL};
-static const char *previous[] = {"/usr/bin/playerctl", "previous", NULL};
-static const char *playpause[] = {"/usr/bin/playerctl", "play-pause", NULL};
-static const char *next[] = {"/usr/bin/playerctl", "next", NULL};
-static const char *goingout[] = {"/usr/bin/nwgbar", "-o", "0.2", "-t",
-                                 "dwm.json",        NULL};
+static char *colors[][ColCount] = {
+    /*                       fg                bg                border float
+     */
+    [SchemeNorm] = {normfgcolor, normbgcolor, normbordercolor, normfloatcolor},
+    [SchemeSel] = {selfgcolor, selbgcolor, selbordercolor, selfloatcolor},
+    [SchemeTitleNorm] = {titlenormfgcolor, titlenormbgcolor,
+                         titlenormbordercolor, titlenormfloatcolor},
+    [SchemeTitleSel] = {titleselfgcolor, titleselbgcolor, titleselbordercolor,
+                        titleselfloatcolor},
+    [SchemeTagsNorm] = {tagsnormfgcolor, tagsnormbgcolor, tagsnormbordercolor,
+                        tagsnormfloatcolor},
+    [SchemeTagsSel] = {tagsselfgcolor, tagsselbgcolor, tagsselbordercolor,
+                       tagsselfloatcolor},
+    [SchemeHidNorm] = {hidnormfgcolor, hidnormbgcolor, c000000, c000000},
+    [SchemeHidSel] = {hidselfgcolor, hidselbgcolor, c000000, c000000},
+    [SchemeUrg] = {urgfgcolor, urgbgcolor, urgbordercolor, urgfloatcolor},
+};
 
-/* tagging */
-static const char *tags[] = {"", "", "", "", "", "", "", "", ""};
+const char *spcmd1[] = {"st", "-n", "spterm", "-g", "120x34", NULL};
+static Sp scratchpads[] = {
+    /* name          cmd  */
+    {"spterm", spcmd1},
+};
 
+/* Tags
+ * In a traditional dwm the number of tags in use can be changed simply by
+ * changing the number of strings in the tags array. This build does things a
+ * bit different which has some added benefits. If you need to change the number
+ * of tags here then change the NUMTAGS macro in dwm.c.
+ *
+ * Examples:
+ *
+ *  1) static char *tagicons[][NUMTAGS*2] = {
+ *         [DEFAULT_TAGS] = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "A",
+ * "B", "C", "D", "E", "F", "G", "H", "I" },
+ *     }
+ *
+ *  2) static char *tagicons[][1] = {
+ *         [DEFAULT_TAGS] = { "•" },
+ *     }
+ *
+ * The first example would result in the tags on the first monitor to be 1
+ * through 9, while the tags for the second monitor would be named A through I.
+ * A third monitor would start again at 1 through 9 while the tags on a fourth
+ * monitor would also be named A through I. Note the tags count of NUMTAGS*2 in
+ * the array initialiser which defines how many tag text / icon exists in the
+ * array. This can be changed to *3 to add separate icons for a third monitor.
+ *
+ * For the second example each tag would be represented as a bullet point. Both
+ * cases work the same from a technical standpoint - the icon index is derived
+ * from the tag index and the monitor index. If the icon index is is greater
+ * than the number of tag icons then it will wrap around until it an icon
+ * matches. Similarly if there are two tag icons then it would alternate between
+ * them. This works seamlessly with alternative tags and alttagsdecoration
+ * patches.
+ */
+static char *tagicons[][NUMTAGS] = {
+    [DEFAULT_TAGS] = {"", "", "", "4", "5", "6", "7", "", ""},
+    [ALTERNATIVE_TAGS] = {"A", "B", "C", "D", "E", "F", "G", "H", "I"},
+    [ALT_TAGS_DECORATION] = {"<1>", "<2>", "<3>", "<4>", "<5>", "<6>", "<7>",
+                             "<8>", "<9>"},
+};
+
+/* There are two options when it comes to per-client rules:
+ *  - a typical struct table or
+ *  - using the RULE macro
+ *
+ * A traditional struct table looks like this:
+ *    // class      instance  title  wintype  tags mask  isfloating  monitor
+ *    { "Gimp",     NULL,     NULL,  NULL,    1 << 4,    0,          -1 },
+ *    { "Firefox",  NULL,     NULL,  NULL,    1 << 7,    0,          -1 },
+ *
+ * The RULE macro has the default values set for each field allowing you to only
+ * specify the values that are relevant for your rule, e.g.
+ *
+ *    RULE(.class = "Gimp", .tags = 1 << 4)
+ *    RULE(.class = "Firefox", .tags = 1 << 7)
+ *
+ * Refer to the Rule struct definition for the list of available fields
+ * depending on the patches you enable.
+ */
 static const Rule rules[] = {
     /* xprop(1):
      *	WM_CLASS(STRING) = instance, class
      *	WM_NAME(STRING) = title
+     *	WM_WINDOW_ROLE(STRING) = role
+     *	_NET_WM_WINDOW_TYPE(ATOM) = wintype
      */
-    /* class      instance    title       tags mask     isfloating   monitor */
-    {"Gimp", NULL, NULL, 0, 1, -1},
-    {"Firefox", NULL, NULL, 1 << 1, 0, -1},
+    RULE(.wintype = WTYPE "DIALOG", .isfloating = 1)
+        RULE(.wintype = WTYPE "UTILITY", .isfloating = 1)
+            RULE(.wintype = WTYPE "TOOLBAR", .isfloating = 1)
+                RULE(.wintype = WTYPE "SPLASH", .isfloating = 1)
+                    RULE(.class = "Gimp", .tags = 1 << 4)
+                        RULE(.class = "Firefox", .tags = 1 << 7)
+                            RULE(.instance = "spterm", .tags = SPTAG(0),
+                                 .isfloating = 1)};
+
+/* Bar rules allow you to configure what is shown where on the bar, as well as
+ * introducing your own bar modules.
+ *
+ *    monitor:
+ *      -1  show on all monitors
+ *       0  show on monitor 0
+ *      'A' show on active monitor (i.e. focused / selected) (or just -1 for
+ * active?) bar - bar index, 0 is default, 1 is extrabar alignment - how the
+ * module is aligned compared to other modules widthfunc, drawfunc, clickfunc -
+ * providing bar module width, draw and click functions name - does nothing,
+ * intended for visual clue and for logging / debugging
+ */
+static const BarRule barrules[] = {
+    /* monitor   bar    alignment         widthfunc                drawfunc
+       clickfunc                name */
+    {-1, 0, BAR_ALIGN_LEFT, width_tags, draw_tags, click_tags, "tags"},
+    {-1, 0, BAR_ALIGN_LEFT, width_ltsymbol, draw_ltsymbol, click_ltsymbol,
+     "layout"},
+    {statusmon, 0, BAR_ALIGN_RIGHT, width_status, draw_status, click_status,
+     "status"},
+    {-1, 0, BAR_ALIGN_NONE, width_wintitle, draw_wintitle, click_wintitle,
+     "wintitle"},
 };
 
 /* layout(s) */
 static const float mfact = 0.55; /* factor of master area size [0.05..0.95] */
 static const int nmaster = 1;    /* number of clients in master area */
 static const int resizehints =
-    1; /* 1 means respect size hints in tiled resizals */
+    0; /* 1 means respect size hints in tiled resizals */
+static const int lockfullscreen =
+    1; /* 1 will force focus on the fullscreen window */
 
 static const Layout layouts[] = {
     /* symbol     arrange function */
-    {"🕸️", tile}, /* first entry is default */
-    {"💭", NULL},       /* no layout function means floating behavior */
-    {"🧐", monocle},
+    {"[]=", tile}, /* first entry is default */
+    {"><>", NULL}, /* no layout function means floating behavior */
+    {"[M]", monocle},
+    {"HHH", grid},
 };
 
 /* key definitions */
-#define MODKEY Mod4Mask
+#define MODKEY Mod1Mask
 #define TAGKEYS(KEY, TAG)                                                      \
   {MODKEY, KEY, view, {.ui = 1 << TAG}},                                       \
       {MODKEY | ControlMask, KEY, toggleview, {.ui = 1 << TAG}},               \
@@ -96,12 +239,12 @@ static const Layout layouts[] = {
 static char dmenumon[2] =
     "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = {
-    "/bin/sh", "-c", "rofi -show drun -config ~/rofi-spotlight/rofi.rasi",
-    NULL};
+    "/bin/sh", "-c",
+    "rofi -show drun -config /home/$USER/rofi-spotlight/rofi.rasi", NULL};
 static const char *termcmd[] = {"alacritty", NULL};
 
 static Key keys[] = {
-    /* modifier                     key        function        argument */
+    /* modifier                     key            function argument */
     {MODKEY, XK_p, spawn, {.v = dmenucmd}},
     {MODKEY | ShiftMask, XK_Return, spawn, {.v = termcmd}},
     {MODKEY, XK_b, togglebar, {0}},
@@ -111,57 +254,46 @@ static Key keys[] = {
     {MODKEY, XK_d, incnmaster, {.i = -1}},
     {MODKEY, XK_h, setmfact, {.f = -0.05}},
     {MODKEY, XK_l, setmfact, {.f = +0.05}},
+    {MODKEY | ShiftMask, XK_j, movestack, {.i = +1}},
+    {MODKEY | ShiftMask, XK_k, movestack, {.i = -1}},
     {MODKEY, XK_Return, zoom, {0}},
     {MODKEY, XK_Tab, view, {0}},
+    {MODKEY | ControlMask, XK_z, showhideclient, {0}},
     {MODKEY | ShiftMask, XK_c, killclient, {0}},
+    {MODKEY | ShiftMask, XK_q, quit, {0}},
     {MODKEY, XK_t, setlayout, {.v = &layouts[0]}},
     {MODKEY, XK_f, setlayout, {.v = &layouts[1]}},
     {MODKEY, XK_m, setlayout, {.v = &layouts[2]}},
     {MODKEY, XK_space, setlayout, {0}},
     {MODKEY | ShiftMask, XK_space, togglefloating, {0}},
-    {MODKEY, XK_0, view, {.ui = ~0}},
-    {MODKEY | ShiftMask, XK_0, tag, {.ui = ~0}},
+    {MODKEY, XK_grave, togglescratch, {.ui = 0}},
+    {MODKEY | ControlMask, XK_grave, setscratch, {.ui = 0}},
+    {MODKEY | ShiftMask, XK_grave, removescratch, {.ui = 0}},
+    {MODKEY, XK_0, view, {.ui = ~SPTAGMASK}},
+    {MODKEY | ShiftMask, XK_0, tag, {.ui = ~SPTAGMASK}},
     {MODKEY, XK_comma, focusmon, {.i = -1}},
     {MODKEY, XK_period, focusmon, {.i = +1}},
     {MODKEY | ShiftMask, XK_comma, tagmon, {.i = -1}},
     {MODKEY | ShiftMask, XK_period, tagmon, {.i = +1}},
-    {MODKEY, XK_minus, setgaps, {.i = -1}},
-    {MODKEY, XK_equal, setgaps, {.i = +1}},
-    {MODKEY | ShiftMask, XK_equal, setgaps, {.i = 0}},
     TAGKEYS(XK_1, 0) TAGKEYS(XK_2, 1) TAGKEYS(XK_3, 2) TAGKEYS(XK_4, 3)
         TAGKEYS(XK_5, 4) TAGKEYS(XK_6, 5) TAGKEYS(XK_7, 6) TAGKEYS(XK_8, 7)
-            TAGKEYS(XK_9, 8){MODKEY | ShiftMask, XK_q, spawn, {.v = goingout}},
-    {0, XF86XK_AudioLowerVolume, spawn, {.v = downvol}},
-    {0, XF86XK_AudioMute, spawn, {.v = mutevol}},
-    {0, XF86XK_AudioRaiseVolume, spawn, {.v = upvol}},
-    {0, XF86XK_MonBrightnessUp, spawn, {.v = briup}},
-    {0, XF86XK_MonBrightnessDown, spawn, {.v = bridown}},
-    {0, XF86XK_AudioPrev, spawn, {.v = previous}},
-    {0, XF86XK_AudioPlay, spawn, {.v = playpause}},
-    {0, XF86XK_AudioNext, spawn, {.v = next}},
-    {0, XK_Print, spawn, {.v = printflame}},
-    {MODKEY, XK_z, spawn, {.v = idleinhibit}},
-    {MODKEY | ShiftMask, XK_z, spawn, {.v = noinhibit}},
-    {MODKEY | ShiftMask, XK_a, spawn,
-     SHCMD("rofi -modi \"clipboard:greenclip print\" -show clipboard "
-           "-run-command '{cmd}'")},
-};
+            TAGKEYS(XK_9, 8)};
 
 /* button definitions */
 /* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle,
  * ClkClientWin, or ClkRootWin */
 static Button buttons[] = {
-    /* click                event mask      button          function argument
-     */
+    /* click                event mask           button          function
+       argument */
     {ClkLtSymbol, 0, Button1, setlayout, {0}},
     {ClkLtSymbol, 0, Button3, setlayout, {.v = &layouts[2]}},
+    {ClkWinTitle, 0, Button1, togglewin, {0}},
+    {ClkWinTitle, 0, Button3, showhideclient, {0}},
     {ClkWinTitle, 0, Button2, zoom, {0}},
     {ClkStatusText, 0, Button2, spawn, {.v = termcmd}},
     {ClkClientWin, MODKEY, Button1, movemouse, {0}},
-    {ClkClientWin, MODKEY | ShiftMask, Button1, movemouse, {.i = 1}},
     {ClkClientWin, MODKEY, Button2, togglefloating, {0}},
     {ClkClientWin, MODKEY, Button3, resizemouse, {0}},
-    {ClkClientWin, MODKEY | ShiftMask, Button3, resizemouse, {.i = 1}},
     {ClkTagBar, 0, Button1, view, {0}},
     {ClkTagBar, 0, Button3, toggleview, {0}},
     {ClkTagBar, MODKEY, Button1, tag, {0}},
